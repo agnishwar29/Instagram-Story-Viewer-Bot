a90fcf141e7b68e52dc5e104ba2254b8f25796b7
Agnishwar Bagchi <agnishwar29@gmail.com>
2022-08-12 19:39:49 +0530

    Second Commit -> Looping over all those accounts now

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ path = r"C:\Program Files (x86)\chromedriver.exe"
 
 driver = webdriver.Chrome(executable_path=path)
 
-username = 'playing_underrated_games'
-password = '19Arka99'
+# username = 'playing_underrated_games'
+# password = '19Arka99'
+
+
+username = 'test_911_test'
+password = 'Agni@Arka911'
 
 
 class StoryViewer:
@@ -20,10 +24,12 @@ class StoryViewer:
     # calling functions from here
     def start(self):
         # Function to log in
-        self.__login()
-
+        try:
+            self.__login()
+            self.__visit_profile([])
+        except:
+            return
         # Function to visit profile, needs a list of usernames but currently not implemented
-        self.__visit_profile([])
 
     # Log in function
     def __login(self):
@@ -62,9 +68,11 @@ class StoryViewer:
 
             print("Successfully Logged in")
 
-        except:
 
+
+        except:
             print("Error Occurred")
+            return
 
     def __visit_profile(self, profiles):
 
@@ -90,24 +98,49 @@ class StoryViewer:
         time.sleep(sleep)
 
         def view_story(*, profile_xpath):
-            driver.find_element(By.XPATH,profile_xpath).click()
+            # getting into profile
+            driver.find_element(By.XPATH, profile_xpath).click()
+
             try:
+                # clicking story
+                # time.sleep(2)
                 driver.find_element(By.XPATH,
                                     '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/main/div/header/div/div/span/img').click()
+                time.sleep(2)
                 print("Clicked")
             except:
-                print("No story")
-                print("Going back")
+                print("No story -- Going back")
                 driver.execute_script("window.history.go(-1)")
                 action = webdriver.ActionChains(driver)
                 action.move_by_offset(10, 20).perform()
 
-        p_xpath = ['/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div/div[2]/div[1]/div/div[1]/div[2]/div[1]/div/div/span/a/span/div',
-                   '/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div/div[2]/div[1]/div/div[2]/div[2]/div[1]/div/div/span/a/span/div',
-                   '/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div/div[2]/div[1]/div/div[3]/div[2]/div[1]/div/div/span/a/span/div']
+        p_xpath = [
+            '/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div/div[2]/div[1]/div/div[1]/div[2]/div[1]/div/div/span/a/span/div',
+            '/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div/div[2]/div[1]/div/div[2]/div[2]/div[1]/div/div/span/a/span/div',
+            '/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div/div[2]/div[1]/div/div[3]/div[2]/div[1]/div/div/span/a/span/div']
 
-        for i in p_xpath:
+        p_xpaths = [f'/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div/div[2]/div[1]/div/div[{i}]/div[2]/div[1]/div/div/span/a/span/div' for i in range(1,50)]
+
+        for i in p_xpaths:
             view_story(profile_xpath=i)
+            # print(i)
+            # driver.find_element(By.XPATH, i).click()
+            # try:
+            #     # clicking story
+            #     time.sleep(2)
+            #     driver.find_element(By.XPATH,
+            #                         '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/main/div/header/div/div/span/img').click()
+            #     time.sleep(5)
+            #     driver.execute_script("window.history.go(-1)")
+            #     time.sleep(1)
+            #     driver.execute_script("window.history.go(-1)")
+            #     print("Clicked")
+            #     continue
+            # except:
+            #     driver.execute_script("window.history.go(-1)")
+            #     action = webdriver.ActionChains(driver)
+            #     action.move_by_offset(10, 20).perform()
+            #     continue
 
 
 viewer = StoryViewer(username, password)
