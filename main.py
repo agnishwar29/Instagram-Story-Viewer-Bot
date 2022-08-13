58e7f852c58c38d0a14a880c07ca1607ae246132
Agnishwar Bagchi <agnishwar29@gmail.com>
2022-08-14 00:59:43 +0530

    Fourth Commit -> Viewing stories and crossing stories, added target_profile [which profile] to get following from. And number of profiles to check on.

--- main.py
+++ main.py
@@ -9,18 +9,19 @@ driver = webdriver.Chrome(executable_path=path)
 
 class StoryViewer:
     # initializing the username and the password field and the url for instagram login page
-    def __init__(self, *, username, password, n_profiles):
+    def __init__(self, *, username, password,target_profile, n_profiles):
         self.__username = username
         self.__password = password
         self.__url = "https://www.instagram.com/"
         self.__profiles = int(n_profiles)
+        self.__target_profile = target_profile
 
     # calling functions from here
     def start(self):
         # Function to log in
         try:
             self.__login()
-            self.__visit_profile([])
+            self.__visit_profile(self.__target_profile)
         except:
             return
 
@@ -33,7 +34,7 @@ class StoryViewer:
         driver.get("https://www.instagram.com/")
 
         # waiting for page to load
-        time.sleep(sleep)
+        time.sleep(2)
 
         # username input
         user_input = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input')
@@ -44,7 +45,7 @@ class StoryViewer:
         password_input.send_keys(self.__password)
 
         # login button
-        time.sleep(sleep)
+        time.sleep(2)
         try:
             driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]').click()
 
@@ -61,13 +62,11 @@ class StoryViewer:
 
             print("Successfully Logged in")
 
-
-
         except:
             print("Error Occurred")
             return
-
-    def __visit_profile(self, profiles):
+    # Going to target profile
+    def __target(self, profile_id):
 
         sleep = 3
 
@@ -75,29 +74,57 @@ class StoryViewer:
         # go to search bar
         search_bar = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]'
                                                    '/div[1]/section/nav/div[2]/div/div/div[2]/input')
-        search_bar.send_keys("arcturuschild")
+        search_bar.send_keys(profile_id)
         time.sleep(sleep)
 
         # click on first account on searchbar
         driver.find_element(By.XPATH,
                             '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/nav/'
                             'div[2]/div/div/div[2]/div[3]/div/div[2]/div/div[1]/a/div/div[2]/div[1]/div').click()
-
         time.sleep(sleep)
 
         # click on followers
         driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/'
                                       'section/main/div/header/section/ul/li[2]/a/div').click()
         time.sleep(sleep)
+
+    def __visit_profile(self, profile):
+
+        self.__target(profile)
+
+        # sleep = 3
+        #
+        # time.sleep(sleep)
+        # # go to search bar
+        # search_bar = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]'
+        #                                            '/div[1]/section/nav/div[2]/div/div/div[2]/input')
+        # search_bar.send_keys("arcturuschild")
+        # time.sleep(sleep)
+        #
+        # # click on first account on searchbar
+        # driver.find_element(By.XPATH,
+        #                     '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/nav/'
+        #                     'div[2]/div/div/div[2]/div[3]/div/div[2]/div/div[1]/a/div/div[2]/div[1]/div').click()
+        #
+        # time.sleep(sleep)
+        #
+        # # click on followers
+        # driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/'
+        #                               'section/main/div/header/section/ul/li[2]/a/div').click()
+        # time.sleep(sleep)
+
+
+
         # account xpath of n number of accounts
         p_xpaths = [
-            f'/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div/div[2]/div[1]/div/div[{i}]/div[2]/div[1]/div/div/span/a/span/div'
+            f'/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div/div[2]/div[' \
+            f'1]/div/div[{i}]/div[2]/div[1]/div/div/span/a/span/div '
             for i in range(1, self.__profiles + 1)]
 
         viewed = 0
 
         for i in p_xpaths:
-
+            time.sleep(1)
             try:
                 # click on next profile
                 driver.find_element(By.XPATH, i).click()
@@ -106,62 +133,44 @@ class StoryViewer:
 
                 # Click on story
                 driver.find_element(By.XPATH,
-                                    '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/main/div/header/div/div/span/img').click()
-
-                # Story like logic should be here
+                                    '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div['
+                                    '1]/section/main/div/header/div/div/span/img').click()
                 time.sleep(3)
+                # Story like logic should be here
+                try:
+                    # Closing story currently
+                    driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/div[3]/button').click()
+                    time.sleep(1)
 
-                # get back to profile
-                driver.execute_script("window.history.go(-1)")
-
-                time.sleep(1)
-
-                # get back to followers list
-                driver.execute_script("window.history.go(-1)")
-
-                time.sleep(2)
-
-                viewed += 1
-                print("Seen a profile ", viewed)
-                continue
-
+                    # Going back two times // there might be a glitch in Instagram
+                    driver.execute_script("window.history.go(-1)")
+                    driver.execute_script("window.history.go(-1)")
+                    viewed +=1
+                    print("Seen Story ")
+                    continue
+                except:
+                    # if there's no story going back
+                    print("No Story")
+                    driver.execute_script("window.history.go(-1)")
+                    time.sleep(1)
+                    viewed += 1
+                    continue
             except:
-                # click on followers button
-                driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/'
-                                              'section/main/div/header/section/ul/li[2]/a/div').click()
-                time.sleep(1)
-                # click on next profile
-                driver.find_element(By.XPATH, i).click()
-
-                time.sleep(2)
-
-                # click on story
-                driver.find_element(By.XPATH,
-                                    '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/main/div/header/div/div/span/img').click()
-
-                # Story like logic should be here
-                time.sleep(3)
-
-                # go back to profile
+                # if any error to open story or profile simply going back
+                print("No Story")
                 driver.execute_script("window.history.go(-1)")
-
-                time.sleep(1)
-
-                # go back to followers list
-                driver.execute_script("window.history.go(-1)")
-
-                time.sleep(2)
                 viewed += 1
-                print("Seen a profile ", viewed)
                 continue
 
+        print(f"{viewed} Profiles Viewed")
+        driver.close()
 
-username = 'playing_underrated_games'
-password = '19Arka99'
+# username = 'playing_underrated_games'
+# password = '19Arka99'
 
-# username = 'test_911_test'
-# password = 'Agni@Arka911'
+username = 'test_911_test'
+password = 'Agni@Arka911'
 
 
-viewer = StoryViewer(username=username, password=password, n_profiles=5)
+viewer = StoryViewer(username=username, password=password,target_profile="agnishwar.art", n_profiles=5)
 viewer.start()
