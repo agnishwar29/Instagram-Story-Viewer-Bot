57d889f71339ffec6837bbbfc9730df951636259
Agnishwar Bagchi <83580277+agnishwar29@users.noreply.github.com>
2022-10-05 13:36:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from AutomateInsta import StoryViewer
 
-username = 'playing_underrated_games'
-password = '19Arka99'
-
-# username = 'test_911_test'
-# password = 'Agni@Arka911'
+username = 'username'
+password = 'password'
 
 
-viewer = StoryViewer(username=username, password=password,target_profile="arcturuschild", n_profiles=10)
+
+viewer = StoryViewer(username=username, password=password,target_profile="profile", n_profiles=10)
 viewer.start()
